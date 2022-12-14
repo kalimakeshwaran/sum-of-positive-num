20218f6e5356c069481eec537317291e7f11cfe5
kalimakeshwaran <makeshvijay735@gmail.com>
2022-12-14 11:43:53 +0530

    add

---
+++ main.js
@@ -0,0 +1,10 @@
+// sum of positive number only//
+var a=parseInt(prompt("enter the length"))
+    b=0
+for(i=1;i<=a;i++){ 
+   var  c=parseInt(prompt("enter the number"))
+    if(c>0){
+    b=b+c
+}
+    }
+console.log(b)
